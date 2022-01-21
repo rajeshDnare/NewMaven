741d9714802d3b64331b3716ac47404c3b509349
rajeshDnare <rajeshnare@gmail.com>
2022-01-21 15:35:16 +0530

    Hi this is rajesh

---
+++ src/test/java/RajeshNare/MavenforTry/TestDemo01.java
@@ -0,0 +1,11 @@
+package RajeshNare.MavenforTry;
+
+import org.testng.annotations.Test;
+
+public class TestDemo01 {
+	@Test
+	public void printing() {
+		System.out.println("Rajesh Nare");
+	}
+
+}
